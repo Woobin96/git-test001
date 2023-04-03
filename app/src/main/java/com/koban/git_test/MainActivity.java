5ab2aceb689e30fe2ea278038fe9d18394542187
wbkong <hmy1219@naver.com>
2023-04-03 13:39:02 +0900

    버튼 클릭 이벤트 추가  - 버튼 클릭시 text 변경

--- app/src/main/java/com/koban/git_test/MainActivity.java
+++ app/src/main/java/com/koban/git_test/MainActivity.java
@@ -3,13 +3,26 @@ package com.koban.git_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Git test");
+            }
+        });
 
 
     }
