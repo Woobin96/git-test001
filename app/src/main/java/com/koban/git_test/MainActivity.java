65a2eccb3a97c45878595bb7340d2eaeb00bddf0
wbkong <hmy1219@naver.com>
2023-04-06 10:28:40 +0900

    오타 수정
    (cherry picked from commit 14d7e63254aeb6bcc7d01ed6acf4cf9e6792ad0e)

--- app/src/main/java/com/koban/git_test/MainActivity.java
+++ app/src/main/java/com/koban/git_test/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {asdasdasdasdasd
+public class MainActivity extends AppCompatActivity {
 
     Button btn;
     TextView textView;
